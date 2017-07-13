31d40233f3814ef7f45443cfedb29cad075d6106
janabo <376921106@qq.com>
2017-07-13 10:53:36 +0800

    滑动显示不全

--- core/src/main/java/com/dk/mp/core/view/scroll/ObservableScrollView.java
+++ core/src/main/java/com/dk/mp/core/view/scroll/ObservableScrollView.java
@@ -2,13 +2,19 @@ package com.dk.mp.core.view.scroll;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ScrollView;
 
 /**
  * Created by cobb on 2017/7/10.
  */
 
-public class ObservableScrollView extends ScrollView{
+public class ObservableScrollView extends ScrollView {
+
+    private GestureDetector mGestureDetector;
+    View.OnTouchListener mGestureListener;
 
     private ScrollViewListener scrollViewListener = null;
 
@@ -23,6 +29,8 @@ public class ObservableScrollView extends ScrollView{
 
     public ObservableScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mGestureDetector = new GestureDetector(context, new YScrollDetector());
+        setFadingEdgeLength(0);
     }
 
     public void setScrollViewListener(ScrollViewListener scrollViewListener) {
@@ -34,6 +42,22 @@ public class ObservableScrollView extends ScrollView{
         super.onScrollChanged(x, y, oldx, oldy);
         if (scrollViewListener != null) {
             scrollViewListener.onScrollChanged(this, x, y, oldx, oldy);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return super.onInterceptTouchEvent(ev) && mGestureDetector.onTouchEvent(ev);
+    }
+
+    // Return false if we're scrolling in the x direction
+    class YScrollDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            if (Math.abs(distanceY) > Math.abs(distanceX)) {
+                return true;
+            }
+            return false;
         }
     }
 }
